48a787c14d0735cb245fb1bb4b3b7b72380938b7
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-06-05 16:27:31 +0500

    Update ShopStock.java

--- task1-strategy-pattern/src/main/java/com/mjc/stage2/ShopStock.java
+++ task1-strategy-pattern/src/main/java/com/mjc/stage2/ShopStock.java
@@ -1,6 +1,8 @@
 package com.mjc.stage2;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ShopStock {
     private final List<Product> productList;
@@ -8,6 +10,7 @@ public class ShopStock {
     public ShopStock(List<Product> productList) {
         this.productList = productList;
     }
-
-    // Write your code here!
+ public List<Product> executeFilteringStrategy(FilteringStrategy filteringStrategy) {
+        return productList.stream().filter(filteringStrategy::filter).collect(Collectors.toList());
+    }
 }
