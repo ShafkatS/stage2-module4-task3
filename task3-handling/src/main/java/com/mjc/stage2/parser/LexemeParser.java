50a3d2fd02d8a7899f52827c24b9bc1405ad447c
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-06-05 17:11:17 +0500

    Update LexemeParser.java

--- task3-handling/src/main/java/com/mjc/stage2/parser/LexemeParser.java
+++ task3-handling/src/main/java/com/mjc/stage2/parser/LexemeParser.java
@@ -1,9 +1,20 @@
 package com.mjc.stage2.parser;
 
-public class LexemeParser {
+import com.mjc.stage2.entity.AbstractTextComponent;
+import com.mjc.stage2.entity.SymbolLeaf;
+import com.mjc.stage2.entity.TextComponent;
+import com.mjc.stage2.entity.TextComponentType;
+
+public class LexemeParser extends AbstractTextParser {
     private static final String LEXEME_REGEX = "\\s+";
-    private static final String WORD_REGEX = "\\w[\\w!=?():]+";
 
-    // Write your code here!
-
+    @Override
+    public void parse(AbstractTextComponent abstractTextComponent, String string) {
+        char[] arr = string.toCharArray();
+        AbstractTextComponent word = new TextComponent(TextComponentType.WORD);
+        for (char ch : arr) {
+            word.add(new SymbolLeaf(TextComponentType.WORD, ch));
+        }
+        abstractTextComponent.add(word);
+    }
 }
