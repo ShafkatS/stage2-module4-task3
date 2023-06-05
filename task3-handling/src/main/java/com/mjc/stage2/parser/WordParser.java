2270d603dacc8cfc3ee64f716d780c3a47fe94b3
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-06-05 17:11:39 +0500

    Update WordParser.java

--- task3-handling/src/main/java/com/mjc/stage2/parser/WordParser.java
+++ task3-handling/src/main/java/com/mjc/stage2/parser/WordParser.java
@@ -1,8 +1,24 @@
 package com.mjc.stage2.parser;
 
+import com.mjc.stage2.entity.AbstractTextComponent;
 
-public class WordParser {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-    // Write your code here!
+public class WordParser extends AbstractTextParser {
+    private static final String WORD_REGEX = "\\w[\\w!=?():]+";
 
+    @Override
+    public void parse(AbstractTextComponent abstractTextComponent, String string) {
+        if (nextParser != null) {
+            Pattern pattern = Pattern.compile(WORD_REGEX);
+            Matcher matcher = pattern.matcher(string);
+            int startPos = 0;
+            while (matcher.find(startPos)) {
+                String word = matcher.group();
+                startPos = matcher.end();
+                nextParser.parse(abstractTextComponent, word);
+            }
+        }
+    }
 }
